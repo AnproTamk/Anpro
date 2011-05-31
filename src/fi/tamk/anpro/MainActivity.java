cd7d1a83139abc9c512a4b488362b2e26db633a9
Jani Keranen <jani_keranen@msn.com>
2011-05-31 10:18:44 +0300

    added mainmenu

---
+++ src/fi/tamk/anpro/MainActivity.java
@@ -0,0 +1,15 @@
+package fi.tamk.anpro;
+
+import fi.tamk.anpro.R;
+import fi.tamk.anpro.R.layout;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
