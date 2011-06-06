d501132338893387cea850c428274434aa606a97
Anssi Kettunen <kettunen.anssi@gmail.com>
2011-06-06 14:50:50 +0300

    jeejee

--- src/fi/tamk/anpro/GameObject.java
+++ src/fi/tamk/anpro/GameObject.java
@@ -23,19 +23,24 @@ public class GameObject extends GfxObject {
 	public int selectionRadius = 0;
 	
 	// Liikkeen muuttujat
-	private int movementSpeed = 0;
-	private int movementAcceleration = 0;
+	private int movementSpeed = 0; // Kuinka monta yksikköä objekti liikkuu kerrallaan. Arvot välillä 0-5
+	private int movementDelay = 0; // Arvot välillä 5-100(ms), mitä suurempi sitä hitaampi kiihtyvyys
+	private int movementAcceleration = 0; // Liikkeen kiihtyminen ja hidastuminen
 	
 	// Suunnan ja kääntymisen muuttujat
 	private int direction = 0; // 0 on suoraan ylöspäin, 90 oikealle
-	private int turningSpeed = 0;
-	private int turningAcceleration = 0;
+	private int turningDelay = 0; // Arvot välillä 5-100(ms), mitä suurempi sitä hitaampi kääntyminen
+	private int turningAcceleration = 0; // Kääntymisen kiihtyvyys
 	private int turningDirection = 0; // 0 ei käänny, 1 vasen, 2 oikea
 	
+	// Tallennetaan aika
+	private long time;
 	
-	public GameObject(GL10 _gl, Context _context, int _id) {
-		super(_gl, _context, _id);
+	/*
+	 public GameObject() {
+		super();
 	}
+	*/	
 	
 	// Tekee osumalaskennat räjähdyksissä (ei kahden objektin osumisessa toisiinsa)
 	public void triggerImpact(int _damage, int _armorPiercing) {
@@ -47,7 +52,44 @@ public class GameObject extends GfxObject {
 		// VIRTUAALINEN
 	}
 	
-	public void updateMovement() {
+	// 
+	public void updateMovement(long _time) {
+		// Lasketaan kääntymisnopeus objektille
+		if (_time - time >= turningDelay) {
+			// Jos objektin kääntymissuunta on ei mihinkään
+			if (turningDirection == 0) {
+				// ÄLÄ TEE MITÄÄN
+			}
+			// Jos objektin kääntymissuunta on vasemmalle
+			else if (turningDirection == 1) {
+				--direction;
+				// 
+				turningDelay = turningDelay - turningAcceleration;
+				// Tekoäly käsittelee turningAccelerationin
+			}
+			// Jos objektin kääntymissuunta on oikealle
+			else if (turningDirection == 2) {
+				++direction;
+				// Tekoäly käsittelee turningAccelerationin
+				turningDelay = turningDelay - turningAcceleration;
+			}
+		}
 		
+		// Lasketaan liikkumisnopeus objektille
+		// Mitä suurempi movementDelay sitä hitaammin objekti liikkuu
+		if (_time - time >= movementDelay) {
+			// Jos objekti liikkuu eteenpäin
+			if (movementSpeed > 0 ) {
+				xf += (movementSpeed * Math.cos(direction)); // Jos objekti liikkuu liian nopeasti -> movementSpeed*kerroin (esim. 0.1)
+				yf += (movementSpeed * Math.sin(direction));
+				// Tekoäly käsittelee movementAccelerationin
+				movementDelay = movementDelay - movementAcceleration;
+			}
+			// Jos objekti ei liiku
+			else {
+				// ÄLÄ TEE MITÄÄN
+			}
+		}
 	}
 }
+
