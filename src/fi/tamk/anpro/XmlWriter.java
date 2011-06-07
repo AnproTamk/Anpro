405350d8f199e0e6add88447c711389f6469082e
unknown <jose.kortelahti@ee.tamk.fi>
2011-06-07 15:20:36 +0300

    Updates to XmlWriter, XmlReader, Options, SettingsActivity and settings.xml

--- src/fi/tamk/anpro/XmlWriter.java
+++ src/fi/tamk/anpro/XmlWriter.java
@@ -39,11 +39,21 @@ public class XmlWriter {
 	
 	/*
 	 * Tämä funktio tallentaa pelin asetukset XML-tiedostoon.
+	 * @param boolean[] settingStates
 	 **/
-	public boolean saveOptions() {
+	public void saveSettings(boolean[] settingStates) {
 		// Luodaan uusi XML-tiedosto pelitallennukselle.
-		File xmlSaveGame = new File(Environment.getExternalStorageDirectory()+"/options.xml");
+		File xmlSaveGame = new File(Environment.getExternalStorageDirectory()+"/settings.xml");
 		
+		String[] stateId = {"0", "0", "0"};
+		
+		for (int i = 0; i < 3; i++) {
+			if (settingStates[i] == false) {
+				stateId[i] = "0";
+			}
+			else
+				stateId[i] = "1";
+		}
 		try {
 			xmlSaveGame.createNewFile();
 		} catch (Exception e) {
@@ -72,26 +82,29 @@ public class XmlWriter {
 			serializer.setFeature("http://xmlpull.org/v1/doc/features.html#indent-output", true);
 			// Asetetaan tagi nimeltä "res"  <--- LUULTAVASTI MUOKATAAN!
 			serializer.startTag(null, "res");
-			// Tästä alkaa xml-tiedoston sisempi osuus.
+			
+			/* 
+			 * Tästä alkaa xml-tiedoston sisempi osuus.
+			 * 
+			 * Kirjoitetaan settings.xml-tiedostoon elementit 'particles',
+			 * 'music' ja 'sounds', jotka pitävät tiedon siitä, onko partikkelit 
+			 * käytössä vai ei.
+			 */
 			
 			/*
-			 * Kirjoitetaan settings.xml-tiedostoon elementti 'particles',
-			 * joka pitää tiedon siitä, onko partikkelit käytössä vai ei.
+			 * 'Particles'-elementin sisällä on attribuutti, jonka nimenä on 'value'.
+			 * 'Value' tallentaa string-tiedon siitä, onko partikkelit käytössä vai ei.
 			 */
 			serializer.startTag(null, "particles");
-			/*
-			 * 'Particles'-elementin sisällä on attribuutti, jonka nimenä on 'value'.
-			 * 'Value' tallentaa numeerisen tiedon siitä, onko partikkelit käytössä vai ei.
-			 */
-			serializer.attribute(null, "value", "0");
+			serializer.attribute(null, "value", stateId[0]);
 			serializer.endTag(null, "particles");
 			
 			serializer.startTag(null, "music");
-			serializer.attribute(null, "value", "0");
+			serializer.attribute(null, "value", stateId[1]);
 			serializer.endTag(null, "music");
 			
 			serializer.startTag(null, "sounds");
-			serializer.attribute(null, "value", "0");
+			serializer.attribute(null, "value", stateId[2]);
 			serializer.endTag(null, "sounds");
 			
 			serializer.endTag(null, "res");
@@ -105,7 +118,6 @@ public class XmlWriter {
 			e.printStackTrace();
 		}
 		
-		return false;
 	}
 	
 	/*
