6b2130e7007360e85483ef21583c9be124d6fc2b
Matti Lahdeniemi <matti.lahdeniemi@gmail.com>
2011-06-09 15:40:17 +0300

    Changes to HUD.

--- src/fi/tamk/anpro/HUD.java
+++ src/fi/tamk/anpro/HUD.java
@@ -9,69 +9,100 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 
-
 /* 
  * - Pitää sisällään pelinaikaisten valikkojen toiminnallisuuden. 
  *   Grafiikat ovat renderöijässä, mutta HUD-luokka hallitsee kosketuksien 
  *   tarkistamisen, pistelaskurin päivittämisen ja cooldownien näyttämisen.
  *   
  * - 
+ *   
  */
 
 /*
  * 
- *	- Pitää sisällään guiobjekteja, 
+ * - Pitää sisällään guiobjekteja, 
  *    tunnistaa niiden tapahtumat ja
  *    ohjaa niiden funktioihin.
- *    
+ * 
  */
-
 
 public class HUD
 {
-
+	public static final int BUTTON_DEFAULT_WEAPON = 1;
+	public static final int BUTTON_1 			  = 2;
+	public static final int BUTTON_2 			  = 3;
 	
-	// Muuttujat
-	private static HUD instance = null;
+	private static HUD instance   = null;
+	public int[] weapons;
 
-
-
-
-
+	public GuiObject gameScore    = null;
+	public GuiObject cooldownBar1 = null;
+	public GuiObject gunButton1   = null;
+	public GuiObject gunButton2   = null;
+	
+	WeaponStorage weaponStorage  = null;
+	
 	// HUD:in rakentaja
-	protected HUD()
+	public HUD()
 	{
-		
-	}
-	// HUD:in rakentajan loppu
 
-	// Luo HUD:in, jos sellaista ei vielä ole tehty
+	}
+
 	public static HUD getInstance()
 	{
-	    if(instance == null) {
+	    if(instance == null){
 	       instance = new HUD();
 	    }
 	    return instance;
 	}
+	// HUD:in rakentajan loppu
 
 
-	public void handleTouch (int x, int y)
+
+	public void upDateCooldowns(WeaponStorage _weaponStorage)
 	{
+		_weaponStorage = weaponStorage;
+		int coolConsume    = weaponStorage.cooldownLeft[0];
+		int coolLeft   = weaponStorage.cooldownLeft[0];
+		long coolTime;
+		
+		// jos cooldown > 0, cooldown laskee
+		for(coolLeft = coolConsume; coolConsume>0; coolConsume--){
+	        --coolLeft;
+	        	        
+	        // odottaa ajastetusti, koska laskee cooldownia
+	        //coolTime = android.os.SystemClock.uptimeMillis();
+		}
+		
+		// kun cooldown = 0, ilmoitetaan WeaponStorageen, että kyky on taas käytössä
+		if(coolLeft == 0){
+			++coolLeft;
+		}
+	}
+
+	
+	 
+	public void triggerClick(int _buttonId)
+	{
+		
+		if(_buttonId == BUTTON_DEFAULT_WEAPON){
+			weaponStorage.currentWeapon = 0;
+		}
+
+		else if(_buttonId == BUTTON_1){
+			weaponStorage.currentWeapon = 1;
+		}
+
+		else if(_buttonId == BUTTON_2){
+			weaponStorage.currentWeapon = 2;
+		}
+
 		// - Tunnistaa painamistapahtuman ja tulkitsee, tapahtuuko se 
 		//   näytön vai HUD:n alueella.
+
 		//
 		//   -> jos tunnistaa kosketuksen HUD:iin, ohjaa eteenpäin
 		//
-	
-		/*
-		 * 		if(){
-		 * 			
-		 * 		}
-		 * 	
-		 */
-		
-		
 	}
-
 
 }
