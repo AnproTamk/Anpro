5ef90ffe52bc8d3ccb3923aeccd1f2be569a561b
Anssi Kettunen <kettunen.anssi@gmail.com>
2011-06-13 08:38:40 +0300

    Added GenericAi

---
+++ src/fi/tamk/anpro/GenericAi.java
@@ -0,0 +1,16 @@
+package fi.tamk.anpro;
+
+
+abstract public class GenericAi {
+	
+	/*
+	 * Rakentaja
+	 */
+	public GenericAi(int _id) {}
+	
+	/*
+	 * Käsittelee tekoälyn
+	 */
+	abstract public void handleAi();
+	
+}
