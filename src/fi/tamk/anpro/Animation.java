cd24b4a9698540e3dee749ee7ba2adaf530a0395
Mikael Juhala <mikael.juhala@ee.tamk.fi>
2011-06-17 12:01:17 +0300

    Added comments.

--- src/fi/tamk/anpro/Animation.java
+++ src/fi/tamk/anpro/Animation.java
@@ -11,19 +11,21 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.opengl.GLUtils;
 
-public class Animation {
-    // Frames
-    public int[] frames;
-    public int   length;
-    
-    // Tekstuurin mitat
+/**
+ * Sis‰lt‰‰ yhden animaation tekstuurit.
+ */
+public class Animation
+{
+    /* Kuvaruutujen tiedot */
+    public  int[] frames;
+    public  int   length;
     private float imageSize = 0;
     
-    // Puskuri ja taulukko vektoreita varten
+    /* Puskuri ja taulukko vektoreita varten */
     public FloatBuffer vertexBuffer;
     public float[] vertices;
 
-    // Puskuri ja taulukko tekstuuria varten
+    /* Puskuri ja taulukko tekstuuria varten */
     private FloatBuffer textureBuffer;
     private float texture[] = {
         0.0f, 1.0f,		// yl‰vasen
@@ -32,22 +34,30 @@ public class Animation {
         1.0f, 0.0f		// alaoikea
     };
 
-    /*
-     * Rakentaja
+    /**
+     * Alustaa luokan muuttujat ja kutsuu loadFramea jokaista ruutua varten.
+     * 
+     * @param GL10    OpenGL-konteksti
+     * @param Context Ohjelman konteksti
+     * @param String  Tekstuurin tunnus
+     * @paran int     Animaation pituus
      */
-    public Animation(GL10 _gl, Context context, String _id, int _length) {
+    public Animation(GL10 _gl, Context context, String _id, int _length)
+    {
         frames = new int[_length];
         length = _length;
 
+        // Ladataan tekstuurit
         try {
             String idField = _id + "_anim_";
             for (int i = 0; i < frames.length; ++i) {
                 loadFrame(_gl, context, frames, i, R.drawable.class.getField(idField+"_"+i).getInt(getClass()));
             }
         } catch (Exception e) {
-            // Vituiks meni
+            // TODO: K‰sittele t‰m‰
         }
         
+        // M‰‰rit‰ vektorit
         vertices = new float[12];
         vertices[0] = (-1)*imageSize;
         vertices[1] = vertices[0];
@@ -62,13 +72,13 @@ public class Animation {
         vertices[10] = imageSize;
         vertices[11] = 0.0f;
         
+        // Varaa muistia vektoreita varten ja sijoita ne puskureihin
         ByteBuffer byteBuffer = ByteBuffer.allocateDirect(vertices.length * 4);
         byteBuffer.order(ByteOrder.nativeOrder());
         vertexBuffer = byteBuffer.asFloatBuffer();
         vertexBuffer.put(vertices);
         vertexBuffer.position(0);
         
-        // Sama kuin ylh‰‰ll‰, mutta tekstuurille vektoreiden sijaan.
         byteBuffer = ByteBuffer.allocateDirect(vertices.length * 4);
         byteBuffer.order(ByteOrder.nativeOrder());
         textureBuffer = byteBuffer.asFloatBuffer();
@@ -76,11 +86,18 @@ public class Animation {
         textureBuffer.position(0);
     }
 
-    /*
-     * Lataa framen tekstuurin muistiin
+    /**
+     * Lataa yhden tekstuurin muistiin.
+     * 
+     * @param GL10    OpenGL-konteksti
+     * @param Context Ohjelman konteksti
+     * @param int[]   Taulukko tekstuuria varten
+     * @paran int     Kuvaruudun j‰rjestysnumero
+     * @param int     Tekstuurin tunnus resursseissa
      */
     public final void loadFrame(GL10 gl, Context context, int[] var, int offset, int id)
     {
+    	// Ladataan bitmap muistiin
         Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), id);
         
         // Asetetaan mitat
@@ -88,6 +105,7 @@ public class Animation {
             imageSize = (float)bitmap.getWidth();
         }
 
+        // Muunnetaan bitmap OpenGL-tekstuuriksi
         gl.glGenTextures(1, var, 0);
         gl.glBindTexture(GL10.GL_TEXTURE_2D, var[offset]);
 
@@ -96,41 +114,41 @@ public class Animation {
 
         GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bitmap, 0);
 
+        // Tyhjennet‰‰n bitmap muistista
         bitmap.recycle();
     }
     
-    /*
-     * Piirt‰‰ framen ruudulle
+    /**
+     * Piirt‰‰ framen ruudulle.
+     * 
+     * @param GL10  OpenGL-konteksti
+     * @param float X-koordinaatti
+     * @param float Y-koordinaatti
+     * @param int   Suunta
+     * @param int   Kuvaruudun j‰rjestysnumero
      */
-    public final void draw(GL10 _gl, float _x, float _y, int _direction, int _frame) {
+    public final void draw(GL10 _gl, float _x, float _y, int _direction, int _frame)
+    {
         // Resetoidaan mallimatriisi
         _gl.glLoadIdentity();
         
-        // Siirret‰‰n mallimatriisia (X, Y, Z)
+        // Siirret‰‰n ja k‰‰nnet‰‰n mallimatriisia
         _gl.glTranslatef(_x, _y, 0);
-        
-        // Valitaan piirrett‰v‰ tekstuuri
         _gl.glBindTexture(GL10.GL_TEXTURE_2D, frames[_frame]);
         
         // Avataan tekstuuri- ja vektoritaulukot k‰yttˆˆn
         _gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
         _gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
         
-        // Valitaan neliˆn n‰ytett‰v‰ puoli. Huom! Vaikka k‰yt‰mme 2D-tekstuureja, ovat
-        // objektit silti 3-ulotteisia. T‰st‰ syyst‰ on valittava objekteista puoli, joka
-        // n‰ytet‰‰n.
+        // Valitaan neliˆn n‰ytett‰v‰ puoli
         _gl.glFrontFace(GL10.GL_CW);
         
-        // Otetaan vektori- ja tekstuuripuskurit k‰yttˆˆn (OpenGL-ymm‰rt‰‰ nyt, miten objektit
-        // on tarkoitus piirt‰‰)
+        // Otetaan vektori- ja tekstuuripuskurit k‰yttˆˆn
         _gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
-        
         _gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, textureBuffer);
     
-        // Piirret‰‰n neliˆ (parametreissa on sana "TRIANGLE", sill‰ neliˆ muodostetaan oikeasti
-        // kahdesta kolmiosta)   
+        // Piirret‰‰n neliˆ ja tekstuuri
         _gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, vertices.length/3);
-    
     
         // Lukitaan tekstuuri- ja vektoritaulukot pois k‰ytˆst‰
         _gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
