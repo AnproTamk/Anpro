54e24fae366eb5528ad9bcaff144ad747f3dd058
unknown <jose.kortelahti@ee.tamk.fi>
2011-06-17 11:08:17 +0300

    changes...

---
+++ src/fi/tamk/anpro/Hud.java
@@ -0,0 +1,90 @@
+package fi.tamk.anpro;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+
+/* 
+ * - Pitää sisällään pelinaikaisten valikkojen toiminnallisuuden. 
+ *   Grafiikat ovat renderöijässä, mutta HUD-luokka hallitsee kosketuksien 
+ *   tarkistamisen, pistelaskurin päivittämisen ja cooldownien näyttämisen.
+ *   
+ * - 
+ *   
+ */
+
+/*
+ * 
+ * - Pitää sisällään guiobjekteja, 
+ *    tunnistaa niiden tapahtumat ja
+ *    ohjaa niiden funktioihin.
+ * 
+ */
+
+public class Hud
+{
+	/* Painikkeiden tunnukset */
+	public static final int BUTTON_1  = 0;
+	public static final int BUTTON_2  = 1;
+	public static final int BUTTON_3  = 2;
+	public static final int SPECIAL_1 = 2;
+	public static final int SPECIAL_2 = 2;
+
+	/* Painikkeisiin sijoitettujen aseiden tunnukset (viittaa WeaponManagerin
+	   asetaulukoiden soluihin */
+	public int[] weapons;
+	
+	/* Käyttöliittymän objektit */
+	public ArrayList<GuiObject> guiObjects = null;
+
+	/* Osoitin WeaponManageriin (HUDin tehtävänä on muuttaa käytössä olevaa
+	   asetta WeaponManagerista) */
+	private final WeaponManager weaponManager;
+	
+	private static Hud pointerToSelf;
+
+	/**
+	 * Alustaa luokan muuttujat ja kutsuu XmlReaderia.
+	 */
+	public Hud(Context _context)
+	{
+		weaponManager = WeaponManager.getConnection();
+		weapons = new int[5];
+		
+		XmlReader reader = new XmlReader(_context );
+		reader.readHud(this);
+	}
+
+	/**
+	 * Päivittää cooldownit (HUD:ssa näkyvät, ei "oikeita" cooldowneja).
+	 */
+	public final void updateCooldowns()
+	{
+		for(int i = 4; i >= 0; --i) {
+			if(weaponManager.cooldownLeft[weapons[i]] > 0 ) {
+				guiObjects.get(i).usedTexture = 1;
+			}
+			else {
+				guiObjects.get(i).usedTexture = 0;
+			}
+		}
+	}
+
+	/**
+	 * Käsittelee napin painalluksen ja asettaa uuden aseen käyttöön WeaponManageriin.
+	 * 
+	 * @param int Painetun napin tunnus
+	 */
+	public final void triggerClick(int _buttonId)
+	{
+		// Tarkistetaan, onko aseessa cooldownia jäljellä vai ei
+		if (weaponManager.cooldownLeft[weapons[_buttonId]] <=0 ) {
+			weaponManager.currentWeapon = weapons[_buttonId];
+		}
+	}
+	
+	public final static Hud getConnection()
+	{
+		return pointerToSelf;
+	}
+}
