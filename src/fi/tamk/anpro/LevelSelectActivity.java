b2cb8b6d86ac3472d3df3fea93f6027d0db3b290
Matti Lahdeniemi <matti.lahdeniemi@gmail.com>
2011-06-21 15:31:33 +0300

    More checkboxes :)

--- src/fi/tamk/anpro/LevelSelectActivity.java
+++ src/fi/tamk/anpro/LevelSelectActivity.java
@@ -55,7 +55,7 @@ public class LevelSelectActivity extends Activity implements OnItemClickListener
 */
 	            // storyButton.setVisibility(View.VISIBLE);
 	            Toast.makeText(LevelSelectActivity.this, "level" + " " + (position + 1), Toast.LENGTH_LONG).show();
-	        }
+	            }
 	    });
 	}
 		/* OPTIMOINNIN HOMMIA!!!!!!!
@@ -71,58 +71,6 @@ public class LevelSelectActivity extends Activity implements OnItemClickListener
 		}
 		*/
 
-		// View level_1Button = findViewById(R.id.button1);
-
-       /*
-		View level1_Button = findViewById(R.id.button_level1);
-        level1_Button.setOnClickListener(this);
-        
-        View level2_Button = findViewById(R.id.button_level2);
-        level2_Button.setOnClickListener(this);
-        
-        View level3_Button = findViewById(R.id.button_level3);
-        level3_Button.setOnClickListener(this);
-        
-        View level4_Button = findViewById(R.id.button_level4);
-        level4_Button.setOnClickListener(this);
-        
-        View level5_Button = findViewById(R.id.button_level5);
-        level5_Button.setOnClickListener(this);
-        
-        View level6_Button = findViewById(R.id.button_level6);
-        level6_Button.setOnClickListener(this);
-        
-        View level7_Button = findViewById(R.id.button_level7);
-        level7_Button.setOnClickListener(this);
-        
-        View level8_Button = findViewById(R.id.button_level8);
-        level8_Button.setOnClickListener(this);
-        
-        View level9_Button = findViewById(R.id.button_level9);
-        level9_Button.setOnClickListener(this);
-        
-        View level10_Button = findViewById(R.id.button_level10);
-        level10_Button.setOnClickListener(this);
-        
-        View level11_Button = findViewById(R.id.button_level11);
-        level11_Button.setOnClickListener(this);
-        
-        View level12_Button = findViewById(R.id.button_level12);
-        level12_Button.setOnClickListener(this);
-        
-        View level13_Button = findViewById(R.id.button_level13);
-        level13_Button.setOnClickListener(this);
-        
-        View level14_Button = findViewById(R.id.button_level14);
-        level14_Button.setOnClickListener(this);
-        
-        View level15_Button = findViewById(R.id.button_level15);
-        level15_Button.setOnClickListener(this);
-        
-        View mainmenuButton = findViewById(R.id.button_mainmenu);
-        mainmenuButton.setOnClickListener(this);    
-	}
-*/
 	    
 	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
 		// TODO Auto-generated method stub
@@ -130,7 +78,7 @@ public class LevelSelectActivity extends Activity implements OnItemClickListener
 	}
 
 	public void onClick(View v) {
-		// TODO Auto-generated method stub
+		// TODO button_story täytyy saada kommunikoimaan gallerian kanssa
 		switch(v.getId()) {
     	case R.id.button_story:
     		SoundManager.playSound(2, 1);
@@ -147,64 +95,5 @@ public class LevelSelectActivity extends Activity implements OnItemClickListener
 		}
 		
 	}
-
-/*	public void onClick(View _v) {
-		switch(_v.getId()) {
-	    	case R.id.button_level1:
-	    		SoundManager.playSound(2, 1);
-	    		Intent i_game = new Intent(this, GameActivity.class);
-	    		startActivity(i_game);
-	    		finish();
-	    		break;
-	    	case R.id.button_level2:
-	    		SoundManager.playSound(2, 1);
-	    		break;
-	    	case R.id.button_level3:
-	    		SoundManager.playSound(2, 1);
-	    		break;
-	    	case R.id.button_level4:
-	    		SoundManager.playSound(2, 1);
-	    		break;
-	    	case R.id.button_level5:
-	    		SoundManager.playSound(2, 1);
-	    		break;
-	    	case R.id.button_level6:
-	    		SoundManager.playSound(2, 1);
-	    		break;
-	    	case R.id.button_level7:
-	    		SoundManager.playSound(2, 1);
-	    		break;
-	    	case R.id.button_level8:
-	    		SoundManager.playSound(2, 1);
-	    		break;
-	    	case R.id.button_level9:
-	    		SoundManager.playSound(2, 1);
-	    		break;
-	    	case R.id.button_level10:
-	    		SoundManager.playSound(2, 1);
-	    		break;
-	    	case R.id.button_level11:
-	    		SoundManager.playSound(2, 1);
-	    		break;
-	    	case R.id.button_level12:
-	    		SoundManager.playSound(2, 1);
-	    		break;
-	    	case R.id.button_level13:
-	    		SoundManager.playSound(2, 1);
-	    		break;
-	    	case R.id.button_level14:
-	    		SoundManager.playSound(2, 1);
-	    		break;
-	    	case R.id.button_level15:
-	    		SoundManager.playSound(2, 1);
-	    		break;
-	    	case R.id.button_mainmenu:
-	    		SoundManager.playSound(2, 1);
-	    		Intent i_mainmenu = new Intent(this, MainActivity.class); // VAATII OPTIMOINTIA - KÄYNNISTÄÄ JOKA KERTA LUOKAN UUDELLEEN
-	    		startActivity(i_mainmenu);
-	    		finish();
-	    		break;  		
-		}
-	}*/
 }
 
