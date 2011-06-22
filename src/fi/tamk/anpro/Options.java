a4e976bb59a1e6056fce7cb5afff1fd071c1fbbe
Anssi Kettunen <kettunen.anssi@gmail.com>
2011-06-22 09:49:00 +0300

    Added scaling for textures

--- src/fi/tamk/anpro/Options.java
+++ src/fi/tamk/anpro/Options.java
@@ -14,10 +14,13 @@ public class Options
 	public static boolean   music;
 	public static boolean[] settings;
 	
+	/* Skaalausmuuttujat */
+	public static float scale;
+
 	/**
 	 * Alustaa luokan muuttujat ja lukee asetukset.
 	 */
-    protected Options()
+    private Options()
     {
     	XmlReader reader = new XmlReader(MainActivity.context);
     	settings = (boolean[])reader.readSettings();
@@ -45,4 +48,39 @@ public class Options
     	music = settings[1];
     	sounds = settings[2];
     }
+    
+	/**
+	 * Asettaa skaalauksen näytön dpi-arvon mukaan ja palauttaa sen
+	 * 
+	 * @param _screenDpi näytön DPI-lukema (320, 240, 160, 120)
+	 */
+	public void scaleConversion(int _screenDpi)
+	{	
+		if (_screenDpi >= 320) {
+			scale = 2.0f;
+		}
+		else if (_screenDpi > 240 && _screenDpi < 320) {
+			scale = 1.5f;
+		}
+		else if (_screenDpi > 160 && _screenDpi <= 240) {
+			scale = 1.0f;
+		}
+		else if (_screenDpi > 120 && _screenDpi <= 160) {
+			scale = 0.75f;
+		}
+		else {
+			System.exit(0);
+			scale = 0.0f;
+		}
+	}
+
+	/**
+	 * Etäisyyksien skaalaus
+	 */
+	public static int pixelConversion(int _pixels)
+	{
+		// Muunnetaan dps:t pikseleiksi tiheyden skaalauksen mukaan ja pyöristetään ylöspäin
+		return ((int)(_pixels * scale + 0.5f));
+	}
+	
 }
