3c186fb768e0dd1e090c4d29543fe1086546a47f
Matti Lahdeniemi <matti.lahdeniemi@gmail.com>
2011-06-29 14:40:22 +0300

    Added stuff to many things.

---
+++ src/fi/tamk/anpro/LinearProjectileAi.java
@@ -0,0 +1,46 @@
+package fi.tamk.anpro;
+
+/**
+ * Toteutus lineaarisen reitinhaun teko‰lylle. Teko‰ly hakeutuu kohteenseensa
+ * mahdollisimman suoraa reitti‰ pitkin ja reagoi ainoastaan kohteen liikkumiseen.
+ * 
+ * K‰ytet‰‰n ainoastaan ammuksille.
+ * 
+ * @extends AbstractAi
+ */
+public class LinearProjectileAi extends AbstractAi
+{
+	/**
+	 * Alustaa luokan muuttujat.
+	 * 
+     * @param int Objektin tunnus piirtolistalla
+     * @param int Objektin tyyppi
+	 */
+	public LinearProjectileAi(int _id, int _type) 
+	{
+		super(_id, _type);
+	}
+
+    /**
+     * Asettaa teko‰lyn aktiiviseksi.
+     * 
+     * @param int X-koordinaatti
+     * @param int Y-koordinaatti
+     */
+	@Override
+    public final void setActive(int _x, int _y)
+    {
+		setDirection(_x, _y);
+		
+		active = true;
+    }
+
+    /**
+     * Asettaa teko‰lyn ep‰aktiiviseksi.
+     */
+	@Override
+    public final void setUnactive()
+    {
+		active = false;
+    }
+}
