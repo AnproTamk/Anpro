ff934acf687955b399fb974780daed11b8fc30fd
Mikael Juhala <mikael.juhala@ee.tamk.fi>
2011-07-04 13:09:29 +0300

    Moved isColliding and isInDamageRadius to Utility class.

--- src/fi/tamk/anpro/GameObject.java
+++ src/fi/tamk/anpro/GameObject.java
@@ -26,10 +26,6 @@ abstract public class GameObject extends GfxObject
     public static final int COLLISION_WITH_PLAYER     = 11;
     public static final int COLLISION_WITH_ENEMY      = 12;
     
-    // Objektin valinta TouchEnginessä
-    public static final int NO_SELECTION = 0;
-    public static final int SELECTABLE   = 1;
-    
     /* Törmäystunnistus */
     public int collisionRadius = 0;
     
@@ -181,46 +177,6 @@ abstract public class GameObject extends GfxObject
     public final void setTurningDelay(float _multiplier)
     {
     	turningDelay = (int) (60 / (_multiplier * speed));
-    }
-    
-    /**
-     * Tarkastaa, tapahtuuko kahden objektin välillä törmäys.
-     * 
-     * @param GameObject Ensimmäinen objekti
-     * @param GameObject Toinen objekti
-     * 
-     * @return boolean Törmäävätkö objektit
-     */
-    public static final boolean isColliding(GameObject _first, GameObject _second)
-    {
-    	float distance = Utility.getDistance(_first.x, _first.y, _second.x, _second.y);
-    	
-        if (distance - _first.collisionRadius - _second.collisionRadius <= 0) {
-           	return true;
-        }
-        else {
-        	return false;
-        }
-    }
-    
-    /**
-     * Tarkastaa, onko kohde vahingon aiheuttaja passiivisen vahingon vaikutusalueella.
-     * 
-     * @param AbstractProjectile Vahingon aiheuttaja
-     * @param GameObject         Kohde
-     * 
-     * @return boolean Vaikuttaako vahinko
-     */
-    public static final boolean isInDamageRadius(AbstractProjectile _attacker, GameObject _target)
-    {
-    	float distance = Utility.getDistance(_attacker.x, _attacker.y, _target.x, _target.y);
-    	
-        if (distance - _attacker.damageRadius - _target.collisionRadius <= 0) {
-           	return true;
-        }
-        else {
-        	return false;
-        }
     }
 }
 
